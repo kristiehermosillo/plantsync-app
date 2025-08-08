32cf9e8c235769749670c309caa29fefa8255827
kristiehermosillo <167710398+kristiehermosillo@users.noreply.github.com>
2025-08-07 19:04:19 -0700

    Update Settings.py

--- pages/Settings.py
+++ pages/Settings.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from utils import load_data, save_data
+from utils import get_theme_css
+
+theme = st.session_state.get("theme", "Light")
+custom_css = get_theme_css(theme)
+st.markdown(custom_css, unsafe_allow_html=True)
 
 st.title("⚙️ Settings")
 
